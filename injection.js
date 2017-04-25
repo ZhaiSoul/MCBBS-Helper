4408f19270af168e33b098c5f28007aa226e56bc
Yanbing Zhao <zzzz@mail.ustc.edu.cn>
2017-04-25 08:40:31 +0800

    Add injection script

---
+++ injection.js
@@ -0,0 +1,8 @@
+// filter urls in images
+[].forEach.call(document.getElementsByTagName('img'), function (img) {
+    img.src = img.src.replace('http://www.mcbbs.net', location.origin)
+});
+// filter urls in links
+[].forEach.call(document.getElementsByTagName('a'), function (a) {
+    a.href = a.href.replace('http://www.mcbbs.net', location.origin)
+});
